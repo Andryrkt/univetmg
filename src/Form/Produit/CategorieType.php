b8a3bb6bfdbbdfa228fec0eb6506db391fca1912
hasinaLtn <andryrkt08@gmail.com>
2025-11-08 06:44:38 +0300

    creation de crud pour le categorie

---
+++ src/Form/Produit/CategorieType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form\Produit;
+
+use App\Entity\Produit\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategorieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('parent', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Categorie::class,
+        ]);
+    }
+}
